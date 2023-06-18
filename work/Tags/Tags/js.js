e80356d34078a18a8801e6a4b38866be5ece668a
djrsdyp <73774806+djrsdyp@users.noreply.github.com>
2023-06-18 10:04:51 +0800

    123

--- work/Tags/Tags/js.js
+++ work/Tags/Tags/js.js
@@ -3,3 +3,9 @@ var countValue = localStorage.getItem('EnglishValue');
 
 // 设置值
 document.getElementById('EnglishValue').textContent = countValue;
+
+// 获取存储的值
+var countValue = localStorage.getItem('CValue');
+
+// 设置值
+document.getElementById('CValue').textContent = countValue;
