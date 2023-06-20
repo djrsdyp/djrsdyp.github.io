c4fb62d77dd682c9fba12546e5daf5994db26a78
djrsdyp <73774806+djrsdyp@users.noreply.github.com>
2023-06-20 12:43:20 +0800

    123

--- work/英语/TXT/css/js.js
+++ work/英语/TXT/css/js.js
@@ -1,17 +1,49 @@
-$(document).ready(function() {
-  $('.image-container img').click(function() {
-    var $image = $(this);
-    
-    // 放大图片
-    $image.css({
-      transform: 'scale(1.3)', /* 放大倍数，可根据需要调整 */
-      zIndex: 9999 /* 确保图片显示在最上层 */
-    });
-    
-    // 允许拖动
-    $image.draggable({
-      cursor: 'move',
-      containment: 'parent' /* 限制拖动范围为父容器 */
-    });
-  });
+var image = document.querySelector('.image-container img');
+var isZoomed = false;
+var startX, startY, translateX, translateY;
+
+image.addEventListener('click', function() {
+  if (isZoomed) {
+    image.classList.remove('zoomed');
+    isZoomed = false;
+  } else {
+    var rect = image.getBoundingClientRect();
+    var scale = Math.min(window.innerWidth * 0.7 / rect.width, window.innerHeight * 0.7 / rect.height);
+    translateX = window.innerWidth / 2 - rect.left - rect.width / 2;
+    translateY = window.innerHeight / 2 - rect.top - rect.height / 2;
+
+    image.style.transform = 'translate(' + translateX + 'px, ' + translateY + 'px) scale(' + scale + ')';
+    image.classList.add('zoomed');
+    isZoomed = true;
+  }
+});
+
+image.addEventListener('mousedown', function(e) {
+  if (isZoomed) {
+    startX = e.clientX - translateX;
+    startY = e.clientY - translateY;
+    image.style.cursor = 'grabbing';
+  }
+});
+
+image.addEventListener('mousemove', function(e) {
+  if (isZoomed && e.buttons === 1) {
+    var newX = e.clientX - startX;
+    var newY = e.clientY - startY;
+    image.style.transform = 'translate(' + newX + 'px, ' + newY + 'px) scale(1.7)';
+    translateX = newX;
+    translateY = newY;
+  }
+});
+
+image.addEventListener('mouseup', function() {
+  if (isZoomed) {
+    image.style.cursor = 'move';
+  }
+});
+
+image.addEventListener('mouseleave', function() {
+  if (isZoomed) {
+    image.style.cursor = 'move';
+  }
 });
