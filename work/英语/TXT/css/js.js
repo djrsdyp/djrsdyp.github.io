57331a6245bac66f9a3edc088acafb44c97d761c
djrsdyp <73774806+djrsdyp@users.noreply.github.com>
2023-06-20 12:59:40 +0800

    123

--- work/英语/TXT/css/js.js
+++ work/英语/TXT/css/js.js
@@ -1,57 +1,39 @@
-var image = document.querySelector('.image-container img');
-var isZoomed = false;
-var startX, startY, translateX, translateY;
+$(document).ready(function() {
+  var scale = 1; // 图片的初始缩放比例
+  var dragStart = { x: 0, y: 0 }; // 记录拖动起始位置
+  var dragging = false; // 是否正在拖动
 
-image.addEventListener('click', function() {
-  if (isZoomed) {
-    // 如果已经放大，则缩小图片
-    image.classList.remove('zoomed');
-    isZoomed = false;
-  } else {
-    var rect = image.getBoundingClientRect();
-    // 计算放大的比例
-    var scale = Math.min(window.innerWidth * 0.7 / rect.width, window.innerHeight * 0.7 / rect.height);
-    // 计算放大后图片的偏移量
-    translateX = window.innerWidth / 2 - rect.left - rect.width / 2;
-    translateY = window.innerHeight / 2 - rect.top - rect.height / 2;
+  // 点击图片触发放大/缩小
+  $('#image').on('click', function() {
+    if (scale === 1) {
+      scale = 2; // 放大到2倍
+      $(this).css('transform', 'scale(' + scale + ')');
+    } else {
+      scale = 1; // 缩小回原始大小
+      $(this).css('transform', 'scale(' + scale + ')');
+    }
+  });
 
-    // 设置放大后的样式
-    image.style.transform = 'translate(' + translateX + 'px, ' + translateY + 'px) scale(' + scale + ')';
-    image.classList.add('zoomed');
-    isZoomed = true;
-  }
-});
+  // 长按图片触发拖动
+  $('#image').on('mousedown', function(event) {
+    dragging = true;
+    dragStart.x = event.pageX - $('#image').position().left;
+    dragStart.y = event.pageY - $('#image').position().top;
+    $('#image').css('cursor', 'grabbing');
+  });
 
-image.addEventListener('mousedown', function(e) {
-  if (isZoomed) {
-    // 如果已经放大，则记录鼠标按下时的起始位置，并设置光标样式为 'grabbing'
-    startX = e.clientX - translateX;
-    startY = e.clientY - translateY;
-    image.style.cursor = 'grabbing';
-  }
-});
+  // 松开鼠标结束拖动
+  $(document).on('mouseup', function() {
+    dragging = false;
+    $('#image').css('cursor', 'grab');
+  });
 
-image.addEventListener('mousemove', function(e) {
-  if (isZoomed && e.buttons === 1) {
-    // 如果已经放大且鼠标按下且移动，则计算新的偏移量，并更新图片位置和偏移量
-    var newX = e.clientX - startX;
-    var newY = e.clientY - startY;
-    image.style.transform = 'translate(' + newX + 'px, ' + newY + 'px) scale(1.7)';
-    translateX = newX;
-    translateY = newY;
-  }
-});
-
-image.addEventListener('mouseup', function() {
-  if (isZoomed) {
-    // 如果已经放大，则恢复光标样式为 'move'
-    image.style.cursor = 'move';
-  }
-});
-
-image.addEventListener('mouseleave', function() {
-  if (isZoomed) {
-    // 如果已经放大，则恢复光标样式为 'move'
-    image.style.cursor = 'move';
-  }
+  // 鼠标移动时进行拖动
+  $(document).on('mousemove', function(event) {
+    if (dragging) {
+      var dragX = event.pageX - dragStart.x;
+      var dragY = event.pageY - dragStart.y;
+      $('#image').css({ top: dragY, left: dragX });
+    }
+  });
 });
