d3e8b9b0f9c593a6e645eccd9919e7a9bd5cb64c
djrsdyp <73774806+djrsdyp@users.noreply.github.com>
2023-06-20 15:05:04 +0800

    123

--- work/英语/TXT/css/js.js
+++ work/英语/TXT/css/js.js
@@ -1,45 +1,19 @@
-var scale = 1; // 图片的初始缩放比例
-    var dragStart = { x: 0, y: 0 }; // 记录拖动起始位置
-    var dragging = false; // 是否正在拖动
-    var imageStart = { x: 0, y: 0 }; // 记录图片起始位置
-
-    // 点击图片触发放大/缩小
-    document.getElementById('image').addEventListener('click', function() {
-      if (scale === 1) {
-        scale = 2; // 放大到2倍
-        this.style.transform = 'scale(' + scale + ')';
-        this.style.cursor = 'grab';
-      } else {
-        scale = 1; // 缩小回原始大小
-        this.style.transform = 'scale(' + scale + ')';
-        this.style.cursor = 'pointer';
-      }
-    });
-
-    // 鼠标按下时记录图片和鼠标的起始位置
-    document.getElementById('image').addEventListener('mousedown', function(event) {
-      dragging = true;
-      imageStart.x = this.offsetLeft;
-      imageStart.y = this.offsetTop;
-      dragStart.x = event.pageX;
-      dragStart.y = event.pageY;
-      this.style.cursor = 'grabbing';
-    });
-
-    // 松开鼠标结束拖动
-    document.addEventListener('mouseup', function() {
-      dragging = false;
-      document.getElementById('image').style.cursor = 'grab';
-    });
-
-    // 鼠标移动时进行拖动
-    document.addEventListener('mousemove', function(event) {
-      if (dragging) {
-        var dragX = event.pageX - dragStart.x;
-        var dragY = event.pageY - dragStart.y;
-        var imageX = imageStart.x + dragX;
-        var imageY = imageStart.y + dragY;
-        document.getElementById('image').style.left = imageX + 'px';
-        document.getElementById('image').style.top = imageY + 'px';
-      }
-    });
+// 在文档加载完成后执行以下代码
+document.addEventListener("DOMContentLoaded", function() {
+  // 获取图片元素
+  var myImage = document.getElementById("my-image");
+  
+  // 初始化 Viewer.js
+  var viewer = new Viewer(myImage, {
+    inline: false, // 是否以内联方式展示
+    button: true, // 是否显示按钮
+    navbar: true, // 是否显示导航栏
+    title: true, // 是否显示标题
+    toolbar: true, // 是否显示工具栏
+    movable: true, // 是否允许拖动
+    zoomable: true, // 是否允许缩放
+    rotatable: true, // 是否允许旋转
+    scalable: true, // 是否允许变焦
+    transition: true, // 是否使用过渡效果
+  });
+});
